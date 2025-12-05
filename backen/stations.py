8d5427dccebd96fa0e4825dced102a55a0b9e9e0
yugalshivaji <yugal.shivaji@gmail.com>
2025-12-05 15:57:37 +0530

    Create stations.py

---
+++ backen/stations.py
@@ -0,0 +1,71 @@
+# A dictionary mapping Station Names to (Latitude, Longitude)
+STATION_MAP = {
+    # --- North Delhi & North-West ---
+    "Alipur": (28.8153, 77.1530),
+    "Ashok Vihar": (28.6953, 77.1816),
+    "Bawana": (28.7762, 77.0510),
+    "Burari Crossing": (28.7256, 77.2011),
+    "DTU": (28.7500, 77.1176),
+    "Jahangirpuri": (28.7328, 77.1706),
+    "Narela": (28.8567, 77.0943),
+    "Rohini": (28.7325, 77.1199),
+    "Wazirpur": (28.6997, 77.1656),
+    
+    # --- South Delhi & South-East ---
+    "Aya Nagar": (28.4706, 77.1099),
+    "CRRI Mathura Road": (28.5512, 77.2735),
+    "Dr. Karni Singh Shooting Range": (28.4985, 77.2647),
+    "Jawaharlal Nehru Stadium": (28.5802, 77.2338),
+    "Lodhi Road": (28.5918, 77.2273),
+    "Major Dhyan Chand Stadium": (28.6117, 77.2374),
+    "Nehru Nagar": (28.5679, 77.2505),
+    "Okhla Phase-2": (28.5307, 77.2712),
+    "Siri Fort": (28.5504, 77.2159),
+    "Sri Aurobindo Marg": (28.5313, 77.1901),
+    
+    # --- East Delhi & North-East ---
+    "Anand Vihar": (28.6476, 77.3158),
+    "Dilshad Garden": (28.6811, 77.2970),
+    "Patparganj": (28.6237, 77.2872),
+    "Sonia Vihar": (28.7105, 77.2592),
+    "Vivek Vihar": (28.6723, 77.3152),
+    
+    # --- West Delhi & South-West ---
+    "Dwarka (Sector 8)": (28.5710, 77.0719),
+    "IGI Airport (T3)": (28.5561, 77.1000),
+    "Mundka": (28.6846, 77.0765),
+    "Najafgarh": (28.6131, 76.9831),
+    "NSIT Dwarka": (28.6090, 77.0325),
+    "Punjabi Bagh": (28.6682, 77.1330),
+    "Pusa": (28.6396, 77.1462),
+    "Shadipur": (28.6514, 77.1473),
+    
+    # --- Central Delhi ---
+    "ITO": (28.6289, 77.2410),
+    "Mandir Marg": (28.6364, 77.1986),
+    "R.K. Puram": (28.5632, 77.1869),
+    
+    # --- NCR: Noida ---
+    "Noida Sector 1": (28.5898, 77.3101),
+    "Noida Sector 62": (28.6251, 77.3788),
+    "Noida Sector 116": (28.5692, 77.3938),
+    "Noida Sector 125": (28.5447, 77.3231),
+    
+    # --- NCR: Ghaziabad ---
+    "Indirapuram": (28.6415, 77.3714),
+    "Loni": (28.7562, 77.2941),
+    "Sanjay Nagar": (28.6853, 77.4445),
+    "Vasundhara": (28.6624, 77.3785),
+    
+    # --- NCR: Gurugram ---
+    "NISE Gwal Pahari": (28.4312, 77.1458),
+    "Gurugram Sector 51": (28.4227, 77.0667),
+    "Teri Gram": (28.4275, 77.1465),
+    "Vikas Sadan": (28.4501, 77.0263),
+    
+    # --- NCR: Faridabad ---
+    "Faridabad NIT": (28.3908, 77.3006),
+    "Faridabad Sector 11": (28.3860, 77.3150),
+    "Faridabad Sector 16A": (28.4088, 77.3178),
+    "Faridabad Sector 30": (28.3683, 77.3228)
+}
